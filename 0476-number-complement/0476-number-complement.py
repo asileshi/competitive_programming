315b4874c8eed72970295450136f1543a692ad64
asileshi <95665894+asileshi@users.noreply.github.com>
2022-11-02 10:51:37 +0300

    Time: 54 ms (55.85%), Space: 13.8 MB (53.45%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -2,8 +2,8 @@ class Solution:
     def findComplement(self, num: int) -> int:
         answer = 0
         for i in range(len(bin(num))-2):
-            if num&(2**i) == 0:
-                answer+=2**i
+            if num&(1<<i) == 0:
+                answer+=1<<i
         return answer
                 
         
