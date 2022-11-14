4cdf2a3a2ca500dbea8ef705448c14ea49f35c6e
asileshi <95665894+asileshi@users.noreply.github.com>
2022-11-14 18:02:43 +0300

    Time: 73 ms (76.12%), Space: 13.9 MB (30.66%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        I = 1
+        V = 5
+        X =10
+        L = 50
+        C = 100
+        D = 500
+        M =1000 
+        rom = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000,'CM':900,'CD':400,'IV':4,'IX':9,'XL':40,'XC':90}
+        ans = 0
+        i = 0
+        while i<len(s):
+            if i+1<len(s) and s[i:i+2] in rom:
+                ans+=rom[s[i:i+2]]
+                i+=2
+            else:
+                ans+=rom[s[i]]
+                i+=1
+        return ans
+            
+        
