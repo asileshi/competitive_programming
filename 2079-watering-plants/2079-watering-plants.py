754052885964d7b65c4229f454eeb0c45a59d259
asileshi <95665894+asileshi@users.noreply.github.com>
2022-11-16 23:38:59 +0300

    Time: 106 ms (45.52%), Space: 14.1 MB (8.77%) - LeetHub

--- 2079-watering-plants/2079-watering-plants.py
+++ 2079-watering-plants/2079-watering-plants.py
@@ -1,13 +1,12 @@
 class Solution:
     def wateringPlants(self, plants: List[int], capacity: int) -> int:
-        ans = 0
-        i = 0
-        while i < len(plants):
+        ans = i = 0
+        while i<len(plants):
             temp = capacity
             while i<len(plants) and temp>=plants[i]:
                 ans+=1
                 temp-=plants[i]
                 i+=1
-            if i<len(plants):
+            if i < len(plants):
                 ans+=2*i
         return ans
