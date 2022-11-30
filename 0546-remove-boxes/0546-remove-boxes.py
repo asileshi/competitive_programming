da49334e7cd61a28263fb8f716b5e9f5546e8f79
asileshi <95665894+asileshi@users.noreply.github.com>
2022-11-30 20:26:43 +0300

    Time: 4408 ms (9.17%), Space: 36.8 MB (11.79%) - LeetHub

--- 0546-remove-boxes/0546-remove-boxes.py
+++ 0546-remove-boxes/0546-remove-boxes.py
@@ -5,12 +5,12 @@ class Solution:
             if l>r:
                 return 0
             while l+1<r and boxes[l] == boxes[l+1]:
-                l+=1
                 c+=1
+                l+=1
             c+=1
             temp = c**2+dp(l+1,r,0)
             for i in range(l+1,r+1):
-                if boxes[l] == boxes[i]:
+                if boxes[i] == boxes[l]:
                     temp = max(temp,dp(i,r,c)+dp(l+1,i-1,0))
             return temp
         return dp(0,len(boxes)-1,0)
