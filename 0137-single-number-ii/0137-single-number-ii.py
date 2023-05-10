8af6029a9d6d8593fecdb9fc36eb6eefed838f6a
asileshi <95665894+asileshi@users.noreply.github.com>
2023-05-10 12:04:10 -0200

    Time: 143 ms (27.76%), Space: 18.1 MB (12.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,7 +1,24 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        ones, twos = 0, 0
-        for num in nums:
-            ones = (ones ^ num) & ~twos
-            twos = (twos ^ num) & ~ones
-        return ones
+    def singleNumber(self, arr: List[int]) -> int:
+        bit_counts = [0] * 32  # assume 32-bit integers
+
+        # Count the number of times each bit is set for all numbers in the array
+        for num in arr:
+            # Handle negative numbers by ignoring the sign bit
+            if num < 0:
+                num = ((1 << 32) - 1) & num
+
+            for i in range(32):
+                bit_counts[i] += (num >> i) & 1
+
+        single_num = 0
+        for i in range(32):
+            if bit_counts[i] % 3 != 0:
+                single_num |= 1 << i
+
+        # Handle negative single number
+        if single_num >= (1 << 31):
+            single_num -= (1 << 32)
+
+        return single_num
+    
