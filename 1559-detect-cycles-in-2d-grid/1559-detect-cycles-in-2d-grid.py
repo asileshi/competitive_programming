1cdbc560333830d5bc93f8a54ca95138a1d39114
asileshi <95665894+asileshi@users.noreply.github.com>
2023-05-19 11:23:54 -0200

    Time: 2466 ms (44.50%), Space: 176.3 MB (52.00%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -4,25 +4,26 @@ class Solution:
         color = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
         def valid(i,j):
             return 0<=i<len(grid) and 0<=j<len(grid[0])
+        
         def dfs(parent,child):
             ci,cj = child
             color[ci][cj] = 1
             for x,y in [(0,1),(1,0),(-1,0),(0,-1)]:
-                newi,newj = x+ci,y+cj
+                newi,newj = ci+x,cj+y
                 if (newi,newj) == parent or not valid(newi,newj):
                     continue
-                if grid[newi][newj] == grid[ci][cj] and color[newi][newj] == 1:
-                    
-                    ans[0] = True
-                    break
-                elif color[newi][newj] == 0 and grid[newi][newj] == grid[ci][cj]:
-                    dfs((ci,cj),(newi,newj))
-            color[ci][cj] = 2
+                if grid[newi][newj] == grid[ci][cj]:
+                    if color[newi][newj] == 1:
+                        ans[0] = True
+                        return 
+                    elif color[newi][newj] == 0:
+                        dfs(child,(newi,newj))
+        
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if color[i][j]==0:
+                if color[i][j] == 0:
                     dfs((-1,-1),(i,j))
-        return ans[0]
+                    if ans[0] == True:
+                        return True
+        return False
                     
-                
-                
