9a8e7c7b8219d206da374baa521ea84d2b7e9f7f
asileshi <95665894+asileshi@users.noreply.github.com>
2023-05-19 11:01:06 -0200

    Time: 4183 ms (5.00%), Space: 184.1 MB (50.00%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        ans = [False]
+        color = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        def valid(i,j):
+            return 0<=i<len(grid) and 0<=j<len(grid[0])
+        def dfs(parent,child):
+            ci,cj = child
+            color[ci][cj] = 1
+            for x,y in [(0,1),(1,0),(-1,0),(0,-1)]:
+                newi,newj = x+ci,y+cj
+                if (newi,newj) == parent or not valid(newi,newj):
+                    continue
+                if grid[newi][newj] == grid[ci][cj] and color[newi][newj] == 1:
+                    
+                    ans[0] = True
+                    break
+                elif color[newi][newj] == 0 and grid[newi][newj] == grid[ci][cj]:
+                    dfs((ci,cj),(newi,newj))
+            color[ci][cj] = 2
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if color[i][j]==0:
+                    dfs((-1,-1),(i,j))
+        return ans[0]
+                    
+                
+                
